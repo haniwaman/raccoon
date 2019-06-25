fcc1925a86501b8842bbd717cf51d6eea3f2605f
haniwaman <haniwa008@gmail.com>
2019-06-25 22:14:10 +0900

    Sass追加

---
+++ parts/infomation.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Infomation
+ *
+ * @package WordPress
+ */
+
+?>
+
+<!-- infomation -->
+<aside class="infomation">
+<div class="infomation-link"><a href="">お問い合わせはこちら！</a></div><!-- /infomation-link -->
+</aside><!-- /infomation -->

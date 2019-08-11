7a5b2075ad75e7b18404cf17adcd27dddb208be9
haniwaman <haniwa008@gmail.com>
2019-08-11 21:57:29 +0900

    カテゴリーと固定ページのパンくずリストを親要素から取得

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -40,7 +40,15 @@ if ( ! function_exists( 'my_breadcrumb' ) ) {
 			if ( 'post' !== get_post_type() ) {
 				$breadcrumb_html .= '<li><a href="' . esc_url( get_post_type_archive_link( get_post_type() ) ) . '">' . esc_html( get_post_type_object( get_post_type() )->labels->name ) . '</a></li>' . $breadcrumb_bridge_tag;
 			} else {
-				$this_categories  = my_get_post_categories( get_the_ID() );
+				$this_categories = my_get_post_categories( get_the_ID() );
+				$thie_parents = get_ancestors( $this_categories[0]['id'], 'category' );
+				if ( $thie_parents ) {
+					$thie_parents = array_reverse( $thie_parents );
+					for ( $i = 0; $i < count( $thie_parents ); $i++ ) {
+						$this_parent_category = get_category( $thie_parents[ $i ] );
+						$breadcrumb_html .= '<li><a href="' . get_category_link( $this_parent_category->term_id ) . '">' . $this_parent_category->cat_name . '</a></li>' . $breadcrumb_bridge_tag;
+					}
+				}
 				$breadcrumb_html .= '<li><a href="' . $this_categories[0]['link'] . '">' . $this_categories[0]['name'] . '</a></li>' . $breadcrumb_bridge_tag;
 			}
 			$breadcrumb_html .= '<li><span class="breadcrumb_current">' . apply_filters( 'raccoon_breadcrumb_title', get_the_title() ) . '</span></li>';
@@ -49,12 +57,27 @@ if ( ! function_exists( 'my_breadcrumb' ) ) {
 
 		} elseif ( is_page() ) { /* 固定ページの場合 */
 			$breadcrumb_html .= $breadcrumb_beore . $breadcrumb_home_tag . $breadcrumb_bridge_tag;
+			$thie_parents = get_ancestors( get_the_ID(), 'page' );
+			if ( $thie_parents ) {
+				$thie_parents = array_reverse( $thie_parents );
+				for ( $i = 0; $i < count( $thie_parents ); $i++ ) {
+					$breadcrumb_html .= '<li><a href="' . get_permalink( $thie_parents[ $i ] ) . '">' . get_the_title( $thie_parents[ $i ] ) . '</a></li>' . $breadcrumb_bridge_tag;
+				}
+			}
 			$breadcrumb_html .= '<li><span class="breadcrumb_current">' . apply_filters( 'raccoon_breadcrumb_title', get_the_title() ) . '</span></li>';
 			$breadcrumb_html .= $breadcrumb_after;
 			echo wp_kses_post( $breadcrumb_html );
 
 		} elseif ( is_category() ) { /* カテゴリーアーカイブの場合 */
 			$breadcrumb_html .= $breadcrumb_beore . $breadcrumb_home_tag . $breadcrumb_bridge_tag;
+			$thie_parents = get_ancestors( get_query_var( 'cat' ), 'category' );
+			if ( $thie_parents ) {
+				$thie_parents = array_reverse( $thie_parents );
+				for ( $i = 0; $i < count( $thie_parents ); $i++ ) {
+					$this_parent_category = get_category( $thie_parents[ $i ] );
+					$breadcrumb_html .= '<li><a href="' . get_category_link( $this_parent_category->term_id ) . '">' . $this_parent_category->cat_name . '</a></li>' . $breadcrumb_bridge_tag;
+				}
+			}
 			$breadcrumb_html .= '<li><span class="breadcrumb_current">' . apply_filters( 'raccoon_breadcrumb_title', single_cat_title( '', false ) ) . '</span></li>';
 			$breadcrumb_html .= $breadcrumb_after;
 			echo wp_kses_post( $breadcrumb_html );

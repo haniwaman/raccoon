d830cce65d96373209ae25b377119d1cf536129f
haniwaman <haniwa008@gmail.com>
2019-08-18 22:18:02 +0900

    commentsのクラス見直し

--- comments.php
+++ comments.php
@@ -10,30 +10,34 @@ if ( post_password_required() || ! comments_open() ) {
 }
 ?>
 
-<!-- comments -->
-<div class="comments">
+<!-- p-comments -->
+<div class="p-comments">
 	<?php if ( have_comments() ) : ?>
-	<div class="comments-title">コメント（<?php echo esc_html( get_comments_number() ); ?>）</div><!-- /comments-title -->
-	<ul class="comments-list">
-		<?php
-		wp_list_comments(
-			array(
-				'avatar_size' => '54',
-				'type'        => 'all', /* all / comment / trackback / pingback / pings */
-			)
-		);
-		?>
-	</ul><!-- /comments-list -->
+	<div class="e-title">コメント（<?php echo esc_html( get_comments_number() ); ?>）</div><!-- /e-title -->
+	<div class="e-list">
+		<ul class="p-comments-list">
+			<?php
+			wp_list_comments(
+				array(
+					'avatar_size' => '54',
+					'type'        => 'all', /* all / comment / trackback / pingback / pings */
+				)
+			);
+			?>
+		</ul><!-- /p-comments-list -->
+	</div><!-- /e-list -->
 	<?php endif; ?>
 
 	<?php if ( get_comment_pages_count() > 1 ) : ?>
-	<nav class="comments-nav">
-		<ul>
-			<li class="comments-prev"><?php previous_comments_link( '前のコメント' ); ?></li>
-			<li class="comments-next"><?php next_comments_link( '次のコメント' ); ?></li>
-		</ul>
-	</nav>
+	<div class="e-nav">
+		<nav class="p-comments-nav">
+			<ul>
+				<li class="e-prev"><?php previous_comments_link( '前のコメント' ); ?></li><!-- /e-prev -->
+				<li class="e-next"><?php next_comments_link( '次のコメント' ); ?></li><!-- /e-next -->
+			</ul>
+		</nav><!-- /p-comments-nav -->
+	</div><!-- /e-nav -->
 	<?php endif; ?>
 
-	<?php comment_form(); ?>
-</div><!-- /comments -->
+	<div class="e-form"><?php comment_form(); ?></div><!-- /e-form -->
+</div><!-- /p-comments -->
